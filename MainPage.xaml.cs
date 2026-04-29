9f5d7315d8631dd357adb875a6c56dccafeefa33
bcadolf <adolfbrandon96@gmail.com>
2026-04-28 22:51:24 -0400

    Well did a folder shuffle to reorganize the project as i learned more about it. got everything working except android device selection.

--- MainPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PersonalFinanceApp;
-
-public partial class MainPage : ContentPage
-{
-	public MainPage()
-	{
-		InitializeComponent();
-	}
-}
---
+++ PersonalFinanceApp.Maui/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PersonalFinanceApp.Maui;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
